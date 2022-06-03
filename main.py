cf92f84286f20856a0c2b21ca66300de9df7536d
Zilan <zilanyaslak77@gmail.com>
2022-06-03 22:12:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("WELCOME LADIES AND GENTLEMEN")
+
+playing = input("Do you want to play this game? ")
+
+if playing != "yes":
+    quit(playing)
+
+print("Okay! LETS HAVE FUN HEHE")
+
+answer = input("Are you happy? ").lower()
+if answer =="yes":
+    print("I am so proud of you!")
+else:
+    print("i hope you feel better soon, I know you will!")
+
+answer = input("Do you like libras? ").lower()
+if answer == "yes":
+    print("You might be a Sag Congrats you got horrible taste in Star Signs!")
+else:
+    print("lmaoo fck outta here")
+
+answer = input("Do you have an Iphone or Android? ").lower()
+if answer == "Iphone":
+    print("AWESOME TASTE! THANK U SO MUCH FOR DOING THIS QUIZ")
+else:
+    print("LOOSER")
+
